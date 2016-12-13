0f913c877199364de4262aea0d4f02ad1c250a4a
kreshermoo <cdeantonio@fiscalcs.com>
2016-12-13 15:56:41 -0500

    December 13 homework

---
+++ main.js
@@ -0,0 +1,127 @@
+//Exercise One
+console.log("Exercise One");
+
+function addNumbers(numberA, numberB) {
+  var sum = numberA + numberB;
+  console.log(numberA + numberB);
+  return sum;
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo) // 4
+
+function addNumbers(numberA, numberB) {
+  return numberA + numberB;
+}
+
+var threePlusTwo = addNumbers(3, 2)
+
+console.log(threePlusTwo) // 5
+
+//Exercise Two
+console.log("Exercise Two");
+
+var exclamation = "you've got to be kidding me";
+
+function yell(exclamation){
+  return exclamation.toUpperCase();
+}
+
+function yell10(exclamation){
+  return yell(exclamation);
+}
+
+console.log(yell10(exclamation));
+
+var exclamation = prompt("Please enter your own exclamation.");
+var exponent = prompt("Please rate your level of excitement on a scale of 1 to 100.");
+
+function yellIt(exclamation, exponent){
+  return (exclamation.toUpperCase() + "! ").repeat(exponent);
+}
+
+if(exclamation != "" && exponent != ""){
+  if(exponent <= 100){
+    console.log(yellIt(exclamation, exponent));
+  }
+  else if(exponent > 100){
+    console.log("You should really calm down. Maybe try deep breathing or counting to 10.")
+  }
+}
+else{
+  console.log("You've got to enter to play.")
+}
+
+
+//Exercise Three
+console.log("Exercise Three");
+
+var naughtyList = prompt("Have you done anything naughty this year? Please list your offenses separated by commas.")
+var niceList = prompt("Have you done anything nice this year? Please list your good deeds separated by commas.")
+
+function stringToArray(strings){
+  var arrayOfStrings = strings.split(",");
+  return arrayOfStrings;
+}
+
+naughtyArray = stringToArray(naughtyList);
+niceArray = stringToArray(niceList);
+
+function longest(naughtyArray, niceArray){
+  if(naughtyArray.length > niceArray.length){
+    var message = "Santa says you have not been very nice and should minimize your expectations for Christmas morning."
+    return message;
+  }
+  else if(niceArray.length > naughtyArray.length){
+    var message = "Santa says Christmas should be very merry for you this year! Though of course, virtue is its own reward."
+    return message;
+  }
+  else{
+    var message = "Hmmm -- remember, you might fool the console, but Santa knows . . ."
+    return message;
+  }
+}
+
+console.log(longest(naughtyArray, niceArray));
+
+
+//Exercise Four
+console.log("Exercise Four");
+
+var character = prompt("Please enter a vowel.").toLowerCase();
+var isY = false;
+
+function isVowel(character){
+  var vowelTrue = false;
+
+  if(character === "a" || character === "e" || character === "i" || character === "o" || character === "u"){
+     vowelTrue = true;
+  }
+  else if(character === "y"){
+    isY = true;
+  }
+
+  return vowelTrue;
+}
+
+if(isVowel(character)){
+  console.log("Yep. That's a vowel. Well played.")
+}
+else if(isY){
+  console.log("Ah, very tricky. According to the Oxford dictionary the letter Y can be regarded as both a vowel and a consonant. Clever.")
+}
+else{
+  console.log("Well, no. Try again, dumb dumb.")
+  character = prompt("Please enter a vowel.");
+
+  if(isVowel(character)){
+    console.log("You got it! Sorry for calling you dumb dumb.")
+  }
+  else if(isY){
+    console.log("That's better! According to the Oxford dictionary the letter Y can be regarded as both a vowel and a consonant. Clever.")
+  }
+  else{
+    console.log("Um, never mind.")
+  }
+}
